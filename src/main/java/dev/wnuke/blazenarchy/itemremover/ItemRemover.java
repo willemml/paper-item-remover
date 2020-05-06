cb66d9ef5eda4a6f9cb31a6a10303b0abd937161
willem <willem@leit.so>
2020-05-06 18:33:31 +0200

    Add everything.

---
+++ src/main/java/dev/wnuke/blazenarchy/itemremover/ItemRemover.java
@@ -0,0 +1,68 @@
+package dev.wnuke.blazenarchy.itemremover;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+/**
+ * Blazenarchy's Item Remover, automatically removes items from player inventories.
+ *
+ * @author wnuke
+ */
+
+public final class ItemRemover extends JavaPlugin {
+
+    public static final String VERSION = "1.0.0";
+    public static final String PREFIX = ChatColor.RED + "[" + ChatColor.AQUA + "ItemRemover" + ChatColor.RED + "]" + ChatColor.WHITE + " ";
+    public static FileConfiguration CONFIG;
+    public String[] itemsStr;
+    public static ArrayList<ItemStack> itemsArr = new ArrayList<ItemStack>();
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.getCommand("itemremover").setExecutor(new ItemRemoverCommand());
+        CONFIG = this.getConfig();
+        itemsStr = CONFIG.getStringList("itemsToRemove").toArray(new String[0]);
+        itemsArr.clear();
+        for (String i : itemsStr) {
+            try {
+                itemsArr.add(new ItemStack(Material.getMaterial(i)));
+            } catch (NullPointerException err) {
+                getLogger().info(i + " is not a valid item.");
+            }
+        }
+        getServer().getPluginManager().registerEvents(new Remover(), this);
+        getLogger().info("Loaded Item Remover version " + VERSION + " by wnuke.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling Item Remover...");
+    }
+
+    private class ItemRemoverCommand implements CommandExecutor {
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            sender.sendMessage(PREFIX + "List of items that will be removed:");
+            if (itemsStr.length > 0) {
+                for (String item : itemsStr) {
+                    sender.sendMessage("  - " + item);
+                }
+            } else {
+                sender.sendMessage("  There are no items in the list.");
+            }
+            return true;
+        }
+    }
+}
---
+++ src/main/java/dev/wnuke/blazenarchy/itemremover/Remover.java
@@ -0,0 +1,26 @@
+package dev.wnuke.blazenarchy.itemremover;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class Remover implements Listener {
+    @EventHandler
+    public void event(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission("itemremover.ignored")) {
+            checkInv(player.getInventory());
+        }
+    }
+
+    public void checkInv(Inventory inventory) {
+        for (ItemStack ri : ItemRemover.itemsArr) {
+            if (inventory.contains(ri.getType())) {
+                inventory.remove(ri.getType());
+            }
+        }
+    }
+}
